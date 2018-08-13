1786c50e1ffd587fdcb8dd7203ffb81555cba0c1
姚衡 <yxs19870128@gmail.com>
2018-08-13 11:51:14 +0800

    日志

---
+++ logging/file.go
@@ -0,0 +1,49 @@
+package logging
+
+import (
+	"os"
+	"fmt"
+	"time"
+	"log"
+)
+
+var (
+	LogSavePath = "runtime/logs"
+	LogSaveName = "log"
+)
+
+func getLogFilePath() string {
+	return fmt.Sprintf("%s", LogSavePath)
+}
+
+func getLogFileFullPath() string {
+	prefixPath := getLogFilePath()
+	suffixPath := fmt.Sprintf("%s%s.log", LogSaveName, time.Now().Format("yyyy-MM-dd"))
+
+	return fmt.Sprintf("%s%s", prefixPath, suffixPath)
+}
+
+func openLogFile(filePath string) *os.File {
+	_, err := os.Stat(filePath)
+	switch {
+	case os.IsNotExist(err):
+		mkDir(getLogFilePath())
+	case os.IsPermission(err):
+		log.Fatalf("Permission :%v", err)
+	}
+
+	handle, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Fail to OpenFile :%v", err)
+	}
+
+	return handle
+}
+
+func mkDir(filePath string) {
+	dir, _ := os.Getwd()
+	err := os.MkdirAll(dir+"/"+getLogFilePath(), os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+}
