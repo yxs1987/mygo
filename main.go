ab5a3bbfbeaff20a617fd6e5180c3106c1ae3608
yang <Yang-xiaolong@dldata.cn>
2018-09-01 14:08:24 +0800

    websocket

--- main.go
+++ main.go
@@ -1,11 +1,73 @@
 package main
 
 import (
-	"net/http"
+	"demogo/pkg/gosocket"
 	"fmt"
-	"mygo/setting"
+	"github.com/gorilla/websocket"
+	"log"
 	"mygo/router"
+	"mygo/setting"
+	"net/http"
+	"time"
 )
+
+var (
+	upgrader = websocket.Upgrader{
+		// 读取存储空间大小
+		ReadBufferSize: 1024,
+		// 写入存储空间大小
+		WriteBufferSize: 1024,
+		// 允许跨域
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	var (
+		wsConn *websocket.Conn
+		err    error
+		// data []byte
+		conn *gosocket.Connection
+		data []byte
+	)
+	// 完成http应答，在httpheader中放下如下参数
+	if wsConn, err = upgrader.Upgrade(w, r, nil); err != nil {
+		return // 获取连接失败直接返回
+	}
+
+	if conn, err = gosocket.InitConnection(wsConn); err != nil {
+		goto ERR
+	}
+
+	go func() {
+		var (
+			err error
+		)
+		for {
+			// 每隔一秒发送一次心跳
+			if err = conn.WriteMessage([]byte("heartbeat")); err != nil {
+				return
+			}
+			time.Sleep(1 * time.Second)
+		}
+
+	}()
+
+	for {
+		if data, err = conn.ReadMessage(); err != nil {
+			goto ERR
+		}
+		if err = conn.WriteMessage(data); err != nil {
+			goto ERR
+		}
+	}
+
+ERR:
+	// 关闭当前连接
+	conn.Close()
+}
 
 func main() {
 	r := router.InitRouter()
@@ -17,7 +79,12 @@ func main() {
 		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
+
+	http.HandleFunc("/ws", wsHandler)
+	// 监听127.0.0.1:7777
+	err := http.ListenAndServe("0.0.0.0:7777", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe", err.Error())
+	}
 	s.ListenAndServe()
 }
-
-
